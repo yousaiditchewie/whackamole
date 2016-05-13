be82e86ee4841521dd49415cedc55be78d714409
Jacob Allen <jedijake@me.com>
2016-05-13 11:46:58 -0700

    aau when I click start, the game begins

--- js/main.js
+++ js/main.js
@@ -1,1 +1,67 @@
 console.log("linked");
+
+var isPlaying = false;
+var avocado = null;
+var guac = [];
+
+/* BEHAVIOR */
+
+function start() {
+  isPlaying = true;
+}
+
+/*  HELPERS */
+
+function renderState() {
+  if (isPlaying) {
+    console.log("Game is on");
+} else {
+  console.log("Paused");
+}
+
+if (avocado === 0) {
+  console.log("A", "_", "_");
+  console.log("_", "_", "_");
+  console.log("_", "_", "_");
+  } else if (avocado === 1) {
+  console.log("_", "A", "_");
+  console.log("_", "_", "_");
+  console.log("_", "_", "_");
+  } else if (avocado === 2) {
+  console.log("_", "_", "A");
+  console.log("_", "_", "_");
+  console.log("_", "_", "_");
+  } else if (avocado === 3) {
+  console.log("_", "_", "_");
+  console.log("A", "_", "_");
+  console.log("_", "_", "_");
+  } else if (avocado === 4) {
+  console.log("_", "_", "_");
+  console.log("_", "A", "_");
+  console.log("_", "_", "_");
+  } else if (avocado === 5) {
+  console.log("_", "_", "_");
+  console.log("_", "_", "A");
+  console.log("_", "_", "_");
+  } else if (avocado === 6) {
+  console.log("_", "_", "_");
+  console.log("_", "_", "_");
+  console.log("A", "_", "_");
+  } else if (avocado === 7) {
+  console.log("_", "_", "_");
+  console.log("_", "_", "_");
+  console.log("_", "A", "_");
+  } else if (avocado === 8) {
+  console.log("_", "_", "_");
+  console.log("_", "_", "_");
+  console.log("_", "_", "A");
+  }
+}
+
+
+/* INTERACTION */
+
+$("button").on("click", start);
+
+
+
